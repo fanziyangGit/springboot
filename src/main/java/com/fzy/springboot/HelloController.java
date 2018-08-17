59889485a330057377428f7c191ac538542476fc
fanziyang <13020000635@163.com>
2018-08-17 17:13:10 +0800

    Initial commit

---
+++ src/main/java/com/fzy/springboot/HelloController.java
@@ -0,0 +1,30 @@
+package com.fzy.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @Value("${name}")
+    private String name;
+    /**
+     *
+     */
+    @Value("${age}")
+    private String age;
+
+    @RequestMapping("/hello2")
+    /* @ResponseBody */
+    public String toIndex() {
+        final String s = "HELLO WORLD";
+        return "hello";
+
+    }
+
+    ;
+}
---
+++ src/main/java/com/fzy/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.fzy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+
+         ConfigurableApplicationContext run = SpringApplication.run(SpringbootApplication.class, args);
+
+    }
+}
